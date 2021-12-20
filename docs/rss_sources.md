39a11a3ff6e5dce7787cbcb959ab53473076ef5a
Siarhei_Papina <Siarhei_papina@epam.com>
2021-12-20 20:04:08 +0300

    rss-reader, save_to_json, verbose, version work, need to work with setup_tools, cashing --date, colorize

--- docs/rss_sources.md
+++ docs/rss_sources.md
@@ -1,5 +1,10 @@
-"https://news.yahoo.com/rss/" +
-"https://news.google.com/rss/" + without media:content
-"https://www.rt.com/rss/news/" +
-"https://www.theguardian.com/world/rss" +
-"https://habr.com/ru/rss/hubs/all/" + without media:content
+https://news.yahoo.com/rss/             ok
+https://news.google.com/rss/            ok, without media:content
+https://www.rt.com/rss/news/            ok
+https://www.theguardian.com/world/rss   ok
+https://habr.com/ru/rss/hubs/all/       ok, without media:content. Works just when epam VPN disconnected.
+https://rss.dw.com/xml/rss-ru-all       ok
+https://rss.dw.com/xml/rss-ru-discover-ger      ok
+https://www.liga.net/news/all/rss.xml           ok
+https://www.finam.ru/analysis/conews/rsspoint   ok
+https://www.svaboda.org/rss/                    ok
--- rss-reader/rss-reader.py
+++ rss-reader/rss-reader.py
@@ -2,120 +2,149 @@ import argparse
 import json
 import bs4.element
 import requests
-import logging
 import re
 import pathlib
+import logging
 
 from pathlib import Path
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-arguments = argparse.ArgumentParser()
-arguments.version = 'Version 1.0'
-arguments.add_argument("source", help="RSS link", default="https://news.yahoo.com/rss/")
-arguments.add_argument("--version", action="version", help="Print version info")
-arguments.add_argument("--verbose", action="store_true", help="Outputs verbose status messages")
-arguments.add_argument("--json", action="store_true", help="Print result as JSON in stdout")
-arguments.add_argument("--limit", action='store', type=int, default=200,
-                       help="Limit news topics if this parameter provided")
 
-args = arguments.parse_args()
-lim = 200
+class RssReaderClass:
+    _parsed_data = {
+        "title": "",
+        "pubdate": "",
+        "link": "",
+        "media": [],
+    }
+    _list_media = []
+    _flag_json = False
+    _flag_verbose = False
+    _data_path = ''
 
-parsed_data = {
-    "title": "",
-    "pubdate": "",
-    "link": "",
-    "media": [],
-}
-list_media = []
+    def __init__(self):
+        # argparse setup
+        self.arguments = argparse.ArgumentParser()
+        self.arguments.version = 'Version 1.0'
+        self.arguments.add_argument("source", help="RSS link", default="https://news.yahoo.com/rss/")
+        self.arguments.add_argument("--version", action="version", help="Print version info")
+        self.arguments.add_argument("--verbose", action="store_true", help="Outputs verbose status messages")
+        self.arguments.add_argument("--json", action="store_true", help="Print result as JSON in stdout")
+        self.arguments.add_argument("--limit", action='store', type=int, default=200,
+                                    help="Limit news topics if this parameter provided")
+        # logger setup
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        logger_stdout_handler = logging.StreamHandler()
+        logger_file_handler = logging.FileHandler('python_logging.log')
+        logger_stdout_handler.setLevel(logging.WARNING)
+        logger_file_handler.setLevel(logging.INFO)
+        logger_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+        logger_file_handler.setFormatter(logger_formatter)
+        self.logger.addHandler(logger_file_handler)
+        self.logger.addHandler(logger_stdout_handler)
+        self.logger.info('\nlog begin')
 
-file_name = 'null.json'
+    def get_args(self):
+        input_args = self.arguments.parse_args()
 
-if args.verbose:
-    print('log information in job')
+        if input_args.verbose:
+            self._flag_verbose = True
+            logger_stdout_handler = logging.StreamHandler()
+            logger_stdout_handler.setLevel(logging.INFO)
+            self.logger.addHandler(logger_stdout_handler)
+            self.logger.info(f'log information printed to stdout')
 
-if args.limit:
-    print(f'the limit for news topics is {args.limit}')
-    lim = args.limit
+        if input_args.limit:
+            self.logger.info(f'the limit for news topics is {input_args.limit}')
 
-if args.json:
-    curr_time = datetime.now()
-    file_name = (f'{curr_time.date().year}' + '_' + f'{curr_time.date().month}' + '_' + f'{curr_time.date().day}' +
-              '_' + f'{curr_time.time().hour}' + '_' + f'{curr_time.time().minute}' + '_' + f'{curr_time.time().second}' +
-              '.json')
-    dir_path = Path(pathlib.Path(), 'data', file_name)
-    print('news directed to file: ' + f'{dir_path}')
+        if input_args.json:
+            self._flag_json = True
+            curr_time = datetime.now()
+            file_name = (
+                    f'{curr_time.date().year}' + '_' + f'{curr_time.date().month}' + '_' + f'{curr_time.date().day}'
+                    + '_' + f'{curr_time.time().hour}' + '_' + f'{curr_time.time().minute}' + '_' +
+                    f'{curr_time.time().second}' + '.json'
+            )
+            self._data_path = Path(pathlib.Path(), 'data', file_name)
+            self.logger.info('news directed to file: ' + f'{self._data_path}')
 
-def out_parsed_data(list_pd, file_name):
-    if args.json:
-        with open(dir_path, "a") as outfile:
-            json.dump(list_pd, outfile)
-            outfile.write("\n")
-    else:
-        print('\nTitle: ' + list_pd["title"])
-        print('Date: ' + list_pd["pubdate"])
-        print('Link: ' + list_pd["link"])
-        print('Media: ')
-        lis = list_pd['media']
-        for l1 in lis:
-            print(f'{l1}' + '\n')
-        print("\n")
+        return input_args
 
-def parse_feed(news_list, limit):
-    pubdate_pattern = r"pubdate|pubDate"
-    link_pattern = r"http://|https://"
-    media_pattern = r"media:content|enclosure"
+    def _out_parsed_data(self, list_pd):
+        if self._flag_json:
+            with open(self._data_path, "a") as outfile:
+                json.dump(list_pd, outfile)
+                outfile.write("\n")
+            self.logger.info('news saved to file: ' + f'{self._data_path}')
+        else:
+            print('\nTitle: ' + list_pd["title"])
+            print('Date: ' + list_pd["pubdate"])
+            print('Link: ' + list_pd["link"])
+            print('Media: ')
+            lis = list_pd['media']
+            for l1 in lis:
+                print(f'{l1}' + '\n')
+            print("\n")
+            self.logger.info('news printed to console')
 
-    for get_feed in news_list:
-        list_media.clear()
-        parsed_data["title"] = get_feed.title.text
-        for news in get_feed.contents:
-            if type(news) == bs4.element.NavigableString:
-                if re.search(link_pattern, f'{news}', re.IGNORECASE):
-                    parsed_data["link"] = news
-            if type(news) == bs4.element.CData:
-                if re.search(link_pattern, f'{news}', re.IGNORECASE):
-                    parsed_data["link"] = f'{news}'
-            if type(news) == bs4.element.Tag:
-                if news.text:
-                    if re.search(pubdate_pattern, f'{news}', re.IGNORECASE):
-                        parsed_data["pubdate"] = news.text
-                    if re.search(media_pattern, f'{news}', re.IGNORECASE):
-                        if news.attrs:
-                            list_media.append(news.attrs['url'])
-                        else:
-                            ghh = news.text.split("href=")
-                            del ghh[0]
-                            for gh in ghh:
-                                g = gh.split("target")
-                                list_media.append(g[0])
-                        parsed_data["media"] = list_media
-                else:
-                    if re.search(media_pattern, f'{news}', re.IGNORECASE):
-                        ghh = news.attrs['url']
-                        list_media.append(ghh)
-                        parsed_data["media"] = list_media
+    def _parse_feed(self, news_list, limit):
+        pubdate_pattern = r"pubdate|pubDate"
+        link_pattern = r"http://|https://"
+        media_pattern = r"media:content|enclosure"
 
-        if limit > 0:
-            out_parsed_data(parsed_data, file_name)
-            limit -= 1
+        for get_feed in news_list:
+            self._list_media.clear()
+            self._parsed_data["title"] = get_feed.title.text
+            for news in get_feed.contents:
+                if type(news) == bs4.element.NavigableString:
+                    if re.search(link_pattern, f'{news}', re.IGNORECASE):
+                        self._parsed_data["link"] = news
+                if type(news) == bs4.element.CData:
+                    if re.search(link_pattern, f'{news}', re.IGNORECASE):
+                        self._parsed_data["link"] = f'{news}'
+                if type(news) == bs4.element.Tag:
+                    if news.text:
+                        if re.search(pubdate_pattern, f'{news}', re.IGNORECASE):
+                            self._parsed_data["pubdate"] = news.text
+                        if re.search(media_pattern, f'{news}', re.IGNORECASE):
+                            if news.attrs:
+                                self._list_media.append(news.attrs['url'])
+                            else:
+                                news_link = news.text.split("href=")
+                                del news_link[0]
+                                for gh in news_link:
+                                    g = gh.split("target")
+                                    self._list_media.append(g[0])
+                            self._parsed_data["media"] = self._list_media
+                    else:
+                        if re.search(media_pattern, f'{news}', re.IGNORECASE):
+                            new_link = news.attrs['url']
+                            self._list_media.append(new_link)
+                            self._parsed_data["media"] = self._list_media
+
+            if limit > 0:
+                self._out_parsed_data(self._parsed_data)
+                limit -= 1
+
+    def parse(self, link, limit):
+
+        try:
+            resp = requests.get(link)
+        except RuntimeError:
+            self.logger.error('content by link does not exist')
+            return 0
+
+        soup_page = BeautifulSoup(resp.content, "html.parser")
+        news_list = soup_page.findAll("item")
+
+        self._parse_feed(news_list, limit)
+        self.logger.info('parse is successfully done')
+        return 0
 
 
-def parse(link, limit):
-
-    try:
-        resp = requests.get(link)
-    except:
-        return link + ' can not be load'
-
-    soup_page = BeautifulSoup(resp.content, "html.parser")
-    news_list = soup_page.findAll("item")
-
-    parse_feed(news_list, limit)
-
-    return 0
-
-
-#print(parse("https://habr.com/ru/rss/hubs/all/", 3))
-parse(args.source, args.limit)
+if __name__ == '__main__':
+    rss = RssReaderClass()
+    args = rss.get_args()
+    rss.parse(args.source, args.limit)

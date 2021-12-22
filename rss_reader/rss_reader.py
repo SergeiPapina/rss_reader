7d6c6915ad2aea995d8acb1887f40f6b6a0e4234
Siarhei_Papina <Siarhei_papina@epam.com>
2021-12-22 15:47:49 +0300

    setuptools works good, saving json fixed

--- rss_reader/rss_reader.py
+++ rss_reader/rss_reader.py
@@ -4,6 +4,7 @@ import bs4.element
 import requests
 import re
 import pathlib
+import os
 import logging
 
 from pathlib import Path
@@ -21,23 +22,29 @@ class RssReaderClass:
     _list_media = []
     _flag_json = False
     _flag_verbose = False
+    _log_dir = str(Path.home() / 'rss_log')
+    _data_dir = str(Path.home() / 'rss_data')
     _data_path = ''
+    _log_path = ''
 
     def __init__(self):
         # argparse setup
         self.arguments = argparse.ArgumentParser()
-        self.arguments.version = 'Version 1.0'
+        self.arguments.version = 'Version 1.2'
         self.arguments.add_argument("source", help="RSS link", default="https://news.yahoo.com/rss/")
         self.arguments.add_argument("--version", action="version", help="Print version info")
         self.arguments.add_argument("--verbose", action="store_true", help="Outputs verbose status messages")
         self.arguments.add_argument("--json", action="store_true", help="Print result as JSON in stdout")
         self.arguments.add_argument("--limit", action='store', type=int, default=200,
                                     help="Limit news topics if this parameter provided")
+        # check existing data directory, create if needed
+        self._check_dir()
         # logger setup
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.INFO)
         logger_stdout_handler = logging.StreamHandler()
-        logger_file_handler = logging.FileHandler('python_logging.log')
+        self._log_path = Path(self._log_dir, 'rss_log.log')
+        logger_file_handler = logging.FileHandler(self._log_path)
         logger_stdout_handler.setLevel(logging.WARNING)
         logger_file_handler.setLevel(logging.INFO)
         logger_formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
@@ -45,6 +52,12 @@ class RssReaderClass:
         self.logger.addHandler(logger_file_handler)
         self.logger.addHandler(logger_stdout_handler)
         self.logger.info('\nlog begin')
+
+    def _check_dir(self):
+        if not Path(self._data_dir).exists():
+            os.mkdir(self._data_dir)
+        if not Path(self._log_dir).exists():
+            os.mkdir(self._log_dir)
 
     def get_args(self):
         input_args = self.arguments.parse_args()
@@ -67,17 +80,20 @@ class RssReaderClass:
                     + '_' + f'{curr_time.time().hour}' + '_' + f'{curr_time.time().minute}' + '_' +
                     f'{curr_time.time().second}' + '.json'
             )
-            self._data_path = Path(pathlib.Path(), 'data', file_name)
+            self._data_path = Path(self._data_dir, file_name)
             self.logger.info('news directed to file: ' + f'{self._data_path}')
 
         return input_args
 
     def _out_parsed_data(self, list_pd):
         if self._flag_json:
-            with open(self._data_path, "a") as outfile:
-                json.dump(list_pd, outfile)
-                outfile.write("\n")
-            self.logger.info('news saved to file: ' + f'{self._data_path}')
+            try:
+                with open(self._data_path, "a") as outfile:
+                    json.dump(list_pd, outfile)
+                    outfile.write("\n")
+                self.logger.info('news saved to file: ' + f'{self._data_path}')
+            except IndentationError:
+                self.logger.error('JSON file cannot be created')
         else:
             print('\nTitle: ' + list_pd["title"])
             print('Date: ' + list_pd["pubdate"])
--- setup.py
+++ setup.py
@@ -1,18 +1,32 @@
+import pathlib
 from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 with open("requirements/requirements.txt") as f:
     required = f.read().splitlines()
 
 setup(
-    version="1.0",
-    packages=find_packages(where="rss_reader", exclude=["tests"]),
-    install_requires=required,
     name="rss_reader",
+    version="1.2",
+    description="RSS feed reader",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SergeiPapina/rss_reader",
     author="Siarhei_Papina",
     author_email="Siarhei_Papina@epam.com",
-    description="RSS feed reader",
     classifiers=["Programming Language :: Python :: 3"],
-    python_requires=">=3.9",
-    package_dir={"": "rss_reader"},
-    include_package_data=True
+    # packages=find_packages(where="rss_reader", exclude=["tests"]),
+    packages=["rss_reader"],
+    install_requires=required,
+    include_package_data=True,
+    #python_requires=">=3.9",
+    #package_dir={"": "rss_reader"},
+    entry_points={
+        "console_scripts": [
+            "rss_reader=rss_reader.__main__:main"
+        ]
+    }
 )
